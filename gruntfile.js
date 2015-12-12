7afc36129d329c8d9398cb9f5963469b4805dff2
Sean Lon <sean.lon@edge-asia.com>
2015-12-12 10:51:02 +0800

    change footer

--- gruntfile.js
+++ gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
           optimization: 2
         },
         files: {
-          "css/css/custom.css": "less/custom.less" ,// destination file and source file
+          "css/custom.css": "less/custom.less" ,// destination file and source file
         
         }
       }
