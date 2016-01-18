83549966bff2d8223370325b72a0103a3db78685
macvhustle <itswatwedo@gmail.com>
2016-01-18 22:54:27 +0800

    bugs fixes from asana list

--- assets/samples/sample-04/js/custom.js
+++ assets/samples/sample-04/js/custom.js
@@ -1,15 +1,16 @@
-// JavaScript Document
-
-         function initResponsiveHeader() {
-                 var sideslider = $('[data-toggle=collapse-side]');
-                 var sel = sideslider.attr('data-target');
-                 var sel2 = sideslider.attr('data-target-2');
-                 sideslider.click(function(event) {
-                     $(sel).toggleClass('in');
-                     $(sel2).toggleClass('out');
-                 });
-         
-             }
-                  function stickDropdown(_elem){
-               $(_elem).parent().removeClass('open').addClass('open');
-         }
+<!--The dropdown js-->
+// Dropdown
+  $(function(){
+    $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            });
+    });
+	// Dropdown
